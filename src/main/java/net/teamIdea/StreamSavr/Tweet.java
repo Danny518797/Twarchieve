8b1f60963f5ebd002578c925789b432b9e72a485
BEAL <beala@csel-25.csel>
2010-10-31 12:07:03 -0600

    Created Tweet and TweetList classes. Work in progress.

---
+++ src/main/java/net/teamIdea/StreamSavr/Tweet.java
@@ -0,0 +1,21 @@
+package net.teamIdea.StreamSavr;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: beala
+ * Date: Oct 31, 2010
+ * Time: 12:06:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tweet {
+    String statusText; //Text of the tweet.
+    String sid; //Unique status ID.
+    String date; //Date of the tweet.
+
+    public Tweet(String sid, String date, String statusText)
+    {
+        this.statusText = statusText;
+        this.date = date;
+        this.sid = sid;
+    }
+}
---
+++ src/main/java/net/teamIdea/StreamSavr/TweetList.java
@@ -0,0 +1,64 @@
+package net.teamIdea.StreamSavr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: beala
+ * Date: Oct 31, 2010
+ * Time: 11:31:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TweetList {
+
+    String username; //User name of the user.
+    String uid; //Unique user ID.
+
+    List<Tweet> tweetList = new ArrayList();
+    int tweetListLength = 0;
+    
+    /* Returns the tweet object at the nth place in the tweetList, where 'tweetToGet' is set the nth place */
+    public Tweet getTweet(int tweetToGet)
+    {
+        return tweetList.get( tweetToGet );
+    }
+
+    /*
+    * Description: Adds a tweet to the end of the tweetList
+    * Return: Returns the element number of the tweet just added. ie, first element is 0, 2nd is 1, etc.
+    */
+    public int addTweet(Tweet tweetToAdd)
+    {
+        tweetList.add(tweetToAdd);
+
+        ++tweetListLength; //increment the size of the tweetList
+
+        return tweetList.size() - 1; //return the element number of the tweet just added. ie, first element is 0, 2nd is 1, etc.
+    }
+
+    public int getSize()
+    {
+        return tweetListLength;
+    }
+
+    /* User name setter */
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    /* UID setter */
+    public void setUid(String uid)
+    {
+        this.uid = uid;
+    }
+
+    /* tweetList setter */
+    public void setTweetList(List<Tweet> tweetList)
+    {
+        this.tweetList = tweetList;
+    }
+
+
+}
