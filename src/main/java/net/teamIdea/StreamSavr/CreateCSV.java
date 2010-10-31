3744df0b1c07af2562aaf0a17d6b1c2c8332e6ed
Daniel Russell Clement <clemendr@csel-36.csel>
2010-10-31 14:15:43 -0600

    I just created the csv file creater.  Untested so far.

---
+++ src/main/java/net/teamIdea/StreamSavr/CreateCSV.java
@@ -0,0 +1,132 @@
+package net.teamIdea.StreamSavr;
+import twitter4j.Status;
+import twitter4j.Tweet;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.Writer;
+import java.lang.Long;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.lang.StringBuffer;
+
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: clemendr
+ * Date: Oct 31, 2010
+ * Time: 11:30:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreateCSV {
+    private TweetList source;
+    private String sFileName;
+    private Status tweet;
+    private Writer output;
+    private File file;
+
+
+    public String getFilename()
+    {
+        if (file.isFile())
+            return file.getName();
+        else
+            return null;
+    }
+
+    public File getFile()
+    {
+        if (file.isFile())
+            return file;
+        else
+            return null;
+    }
+
+    public String getLocation()
+    {
+        if (file.isFile())
+            return file.getPath();
+        else
+            return null;
+    }
+
+    public boolean deleteFile()
+    {
+        return file.delete();
+    }
+
+    public String createCSV(TweetList data)
+    {
+        source = data;
+        sFileName = source.getUsername();
+        String intermediate;
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+
+        try
+        {
+            file = File.createTempFile((sFileName + ".csv"), ".tmp");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write("Date");
+            output.write(',');
+            output.write("Tweet");
+            output.write('\n');
+
+            for(int i=0; i < source.getSize(); i++)
+            {
+                tweet = source.getTweet(i);
+                intermediate = df.format(tweet.getCreatedAt());
+                output.write(addQuotes(intermediate));
+                output.write(',');
+                intermediate = tweet.getText();
+                output.write(modifyStringForCSV(intermediate));
+                output.write('\n');
+            }
+        }
+        catch (IOException e){
+
+        }
+
+    }
+
+    private String modifyStringForCSV(String text)
+    {
+        StringBuffer modified = new StringBuffer(text);
+        char[] quotes = new char['"'];
+        String Quotes = new String(quotes, 0,1);
+        int currentLocation = 0;
+        int lastLocation = 0;
+        if (modified.indexOf(Quotes) != -1)
+        {
+            while (currentLocation != -1)
+            {
+                currentLocation = modified.indexOf(Quotes,lastLocation);
+                if (currentLocation != -1)
+                {
+                    modified.append(quotes, cgit urrentLocation, 1);
+                }
+                lastLocation = currentLocation+1;
+            }
+        }
+        modified.append(quotes, 0, 1);
+        modified.append('"');
+        return modified.toString();
+        
+    }
+
+    private String addQuotes(String text)
+    {
+        StringBuffer modified = new StringBuffer(text);
+        char[] quotes = new char['"'];
+        modified.append(quotes, 0, 1);
+        modified.append('"');
+        return modified.toString();
+
+    }
+
+
+    
+}
