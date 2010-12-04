fce1d9c8391689552d43a66a5a81bc92ba18d432
BEAL <beala@csel-38.csel>
2010-12-04 14:54:55 -0700

    Commented and cleaned up the ArchiverServlet.

--- src/main/java/net/teamIdea/StreamSavr/servlet/ArchiverServlet.java
+++ src/main/java/net/teamIdea/StreamSavr/servlet/ArchiverServlet.java
@@ -30,50 +30,63 @@ public class ArchiverServlet extends HttpServlet {
     private tweetGetter tweetGet = null;
     private CreateCSV csv = null;
 
-    /* Setters for testing purposes */
+    /* Setters for testing purposes
+    *  CSV and tweetget should both be unset unless the function is being tested.
+    * */
     public void setTweetGet(tweetGetter tweetGet) { this.tweetGet = tweetGet; }
     public void setCSV( CreateCSV csv) { this.csv = csv; }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        /* Get parameter 'c' off the request.
+         * IF 'c' is blank, then that means the servlet needs to archive the tweets and store them to the session.
+         * ELSE the servlet needs to get the tweets out of the session, create a CSV, and then send them to the browser.
+         */
         String c = request.getParameter("c");
         if(c != null && "".equals(c)) {
             Twitter twitter = getTwitter(request);
 
-            List<Status> test = null;
+            //List of tweets (each 'status' object contains a tweet).
+            List<Status> tweets = null;
             try {
+                //For testing purposes.
                 if(tweetGet == null)
                     tweetGet = new tweetGetter();
 
-                test = tweetGet.getAllTweets(twitter, request);
-                request.getSession().setAttribute("TWEETS", test);
+                //Call tweetGet which passes back a list full of all the user's tweets.
+                tweets = tweetGet.getAllTweets(twitter, request);
+                //Store the list to the session. will be pulled out of the session and turned into a CSV later.
+                request.getSession().setAttribute("TWEETS", tweets);
+                //Once everything is done, send back an "okay" message to the browser.
                 ServletOutputStream out = response.getOutputStream();
                 out.write("okay".getBytes());
                 out.flush();
                 out.close();
             } catch ( IllegalStateException e) {
+                //If an exception is thrown, the user probably isn't logged in. send them back to the auth screen
                 response.sendRedirect("/auth");
             }
         } else {
+            //for testing purposes
             if(csv == null)
                 csv = new CreateCSV();
 
-            List<Status> test = (List<Status>) request.getSession().getAttribute("TWEETS");
+            //Get the list of tweets off the session. This was created and stored by the code above.
+            List<Status> tweets = (List<Status>) request.getSession().getAttribute("TWEETS");
 
-            // null out the session at somepoint
+            //Pass tweets to createCSV which passes back a CSV file in byte array form.
+            byte[] csvToSend = csv.createCSV(tweets);
 
-            byte[] csvToSend = csv.createCSV(test);
-            //System.out.println("Location: " + csv.getLocation() + " Filename: " + csv.getFilename());
-
+            //Set the response header so the browser will pop up a download dialog when it recieves the CSV.
             response.setHeader("Content-disposition",
                       "attachment; filename=" +
                       "tweets.csv" );
             response.setContentType("application/csv");
             ServletOutputStream out = response.getOutputStream();
+            //send the csv file.
             out.write(csvToSend);
             out.flush();
             out.close();
         }
-        //request.getRequestDispatcher(ARCHIVER_VIEW).forward(request, response);
     }
 }
